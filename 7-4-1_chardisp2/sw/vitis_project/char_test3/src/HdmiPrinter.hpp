2a1ffac4d6d3dee787e984d3afd08d72c46c1284
motchy <motchy869@gmail.com>
2023-06-03 17:13:55 +0900

    feat: 7-4-2_scroll

---
+++ 7-4-1_chardisp2/sw/vitis_project/char_test3/src/HdmiPrinter.hpp
@@ -0,0 +1,27 @@
+#pragma once
+
+#include <stdint.h>
+#include <xparameters.h>
+
+class HdmiPrinter {
+    public:
+        static constexpr uint8_t CR = 0x0d;
+        static constexpr uint8_t LF = 0x0a;
+        struct CursorPos {
+            uint8_t row;
+            uint8_t col;
+        };
+        HdmiPrinter(const uint8_t numRows, const uint8_t numCols);
+        CursorPos getCursorPos() const {return m_cursorPos;}
+        void setColor(const uint8_t r, const uint8_t g, const uint8_t b);
+        void setInversion(const bool inversion);
+        void setBlink(const bool blink);
+        void printOneChar(const uint8_t charCode);
+
+    private:
+        const uint8_t NUM_ROWS, NUM_COLS;
+        CursorPos m_cursorPos = {0, 0}; /* current cursor position */
+        uint8_t m_r = 0, m_g = 0, m_b = 0;
+        bool m_inversion = false, m_blink = false;
+        void setCursorPos(const uint8_t row, const uint8_t col);
+};
