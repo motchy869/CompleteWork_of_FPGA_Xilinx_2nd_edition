d9780df0eeaef2a0d8c2245114f12b9c3ed4bdc9
motchy <motchy869@gmail.com>
2023-06-03 11:51:13 +0900

    refactor: use bit field to access VRAM

--- 7-4-1_chardisp2/sw/vitis_project/char_test2/src/chartest.cpp
+++ 7-4-1_chardisp2/sw/vitis_project/char_test2/src/chartest.cpp
@@ -5,18 +5,29 @@
 
 volatile uint8_t *const VRAM = reinterpret_cast<volatile uint8_t *>(XPAR_CHARDISP_IP_0_BASEADDR);
 
-FORCE_INLINE void getVramEntry(const int index, uint8_t &r, uint8_t &g, uint8_t &b, uint8_t &charCode) {
-    const uint32_t entry = VRAM[index*4];
-    charCode = entry&0xff;
-    b = (entry>>8)&0xf;
-    g = (entry>>12)&0xf;
-    r = (entry>>16)&0xf;
+#pragma pack(1)
+struct VramEntry {
+    unsigned charCode: 7;
+    unsigned reserved_1: 1;
+    unsigned b: 4;
+    unsigned g: 4;
+    unsigned r: 4;
+    unsigned inversion: 1;
+    unsigned blink: 1;
+    unsigned reserved_0: 2;
+};
+#pragma pack()
+static_assert(sizeof(VramEntry)==3, "VramEntry must be 3 bytes");
+
+FORCE_INLINE VramEntry getVramEntry(const int index) {
+    const uint32_t entry = 0x00ffffff&(*reinterpret_cast<volatile uint32_t *>(&VRAM[index*4]));
+    return *reinterpret_cast<const VramEntry *>(&entry);
 }
 
-FORCE_INLINE void setVramEntry(const int index, const bool blink, const bool inversion, const uint8_t r, const uint8_t g, const uint8_t b, const uint8_t charCode) {
-    VRAM[index*4] = charCode;
-    VRAM[index*4+1] = ((g<<4)&0xf0)|(b&0xf);
-    VRAM[index*4+2] = (VRAM[index*4+2] & 0b11000000) | (blink<<5) | (inversion<<4) | (r&0xf);
+FORCE_INLINE void setVramEntry(const int index, const VramEntry entry) {
+    VRAM[index*4] = entry.charCode;
+    VRAM[index*4+1] = ((entry.g<<4)&0xf0)|(entry.b&0xf);
+    VRAM[index*4+2] = (VRAM[index*4+2] & 0b11000000) | (entry.blink<<5) | (entry.inversion<<4) | (entry.r&0xf);
 }
 
 FORCE_INLINE void setVramChar(const int index, const uint8_t charCode) {
@@ -35,18 +46,24 @@ FORCE_INLINE void copyVramEntry(const int dstIndex, const int srcIndex) {
 
 void showLines(const int numLines) {
     for (int i=0; i<numLines; ++i) {
-        uint8_t charCode, b, g, r;
-        getVramEntry(i, r, g, b, charCode);
-        xil_printf("VRAM[%d]: r=%02x, g=%02x, b=%02x, charCode=%02x\n", i, r, g, b, charCode);
+        const VramEntry entry = getVramEntry(i);
+        xil_printf("VRAM[%d]: r=%02x, g=%02x, b=%02x, charCode=%02x\n", i, entry.r, entry.g, entry.b, entry.charCode);
     }
 }
 
 int main() {
     /* Display characters in various colors. */
     for (int i=0; i<4000; ++i) {
-    	constexpr bool blink = false;
-    	const bool inversion = static_cast<bool>((i>>4)&0b1); /* Invert color every 16 characters. */
-        setVramEntry(i, blink, inversion, (i>>8)&0xf, (i>>4)&0xf, i&0xf, i&0xff);
+        const unsigned int ui = i;
+        const VramEntry entry = {
+            .charCode = ui&0xff,
+            .b = ui&0xf,
+            .g = (ui>>4)&0xf,
+            .r = (ui>>8)&0xf,
+            .inversion = static_cast<bool>((ui>>4)&0b1), /* Invert color every 16 characters. */
+            .blink = false
+        };
+        setVramEntry(i, entry);
     }
 
     showLines(40);
