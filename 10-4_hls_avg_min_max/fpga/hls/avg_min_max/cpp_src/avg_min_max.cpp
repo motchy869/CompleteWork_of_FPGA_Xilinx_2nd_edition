6cbc607fd1cb934f5496c507e2380ca534a2bafb
motchy <motchy869@gmail.com>
2023-08-10 01:05:00 +0900

    refactor: reduce redundant memory access

--- 10-4_hls_avg_min_max/fpga/hls/avg_min_max/cpp_src/avg_min_max.cpp
+++ 10-4_hls_avg_min_max/fpga/hls/avg_min_max/cpp_src/avg_min_max.cpp
@@ -4,11 +4,12 @@ void avg_min_max(const q16p2_t a[AVG_MIN_MAX_ARRAY_LENGTH], q16p2_t &avg, q16p2_
     q16p2_t sum = a[0], min_temp = sum, max_temp = sum;
 
     avg_min_max_loop: for (size_t i=1; i<AVG_MIN_MAX_ARRAY_LENGTH; ++i) {
-        sum += a[i];
-        if (a[i] < min_temp) {
-            min_temp = a[i];
-        } else if (a[i] > max_temp) {
-            max_temp = a[i];
+        const q16p2_t a_i = a[i];
+        sum += a_i;
+        if (a_i < min_temp) {
+            min_temp = a_i;
+        } else if (a_i > max_temp) {
+            max_temp = a_i;
         }
     }
 
